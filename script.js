8eca6ce53bd4ca879a0097bd7c1f3e56dd7771dc
Reagan701 <kingreggie702@gmail.com>
2022-05-11 16:30:06 +0200

    Changes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function ChangeBg(){
     var navbar = document.getElementById("navbar");
     var scrollValue = window.scrollY;
-    if(scrollValue <700){
+    if(scrollValue <855){
         navbar.style.background = "transparent";
-    }else if(scrollValue >700 && scrollValue<2000){
-        navbar.style.background = "blue"
     }else{
         navbar.style.background = "white";
     }
