4a5690be9fb81b5a80b1fc6032c8664dad532c91
Reagan <kingreggie702@gmail.com>
2022-05-14 22:37:15 +0200

    Changes to navbar

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+let hasChanged;
+
 function ChangeBg(){
     var navbar = document.getElementById("navbar");
     var scrollValue = window.scrollY;
     var screenwidth = screen.width;
-    if(scrollValue <200 || screenwidth >1024){
-        navbar.style.background = "transparent";
-        navbar.style.border = "0px solid black"
-        navbar.style.margin = "0px";
-        
-    }else{
-        navbar.style.background = "#f8f9fa";
-        navbar.style.border = "2px solid #190844"
-        navbar.style.margin = "10px 40px";
+    if(!beenClicked){
+        if(scrollValue <200 || screenwidth >1024){
+            hasChanged = false;
+            navbar.style.background = "transparent";
+            navbar.style.border = "0px solid black";
+            navbar.style.margin = "0px";
+            
+        }else{
+            hasChanged = true;
+            navbar.style.background = "#f8f9fa";
+            navbar.style.border = "2px solid #190844";
+            navbar.style.margin = "10px 40px";
+        }
     }
+    
     console.log(scrollValue);
+}
+
+let beenClicked = false;
+
+function backgroundClick(){
+    beenClicked = !beenClicked;
+    var navbar = document.getElementById("navbar");
+    var scrollValue = window.scrollY;
+    navbar.style.background = "#f8f9fa";
+    navbar.style.border = "2px solid #190844";
+    navbar.style.margin = "10px 40px";   
+    ChangeBg();
 }
 
 window.addEventListener('scroll', ChangeBg);
