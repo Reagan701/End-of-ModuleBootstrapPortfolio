57c379e40f82b718c32719715861851563faa3f0
Reagan <kingreggie702@gmail.com>
2022-05-14 21:22:59 +0200

    Added Box Shadows, Color Animations and edited margins and sizing

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function ChangeBg(){
     var navbar = document.getElementById("navbar");
     var scrollValue = window.scrollY;
-    if(scrollValue <200){
-        navbar.style.transition = '0.6s';
+    var screenwidth = screen.width;
+    if(scrollValue <200 && screenwidth >1024){
         navbar.style.background = "transparent";
+        navbar.style.border = "0px solid black"
+        navbar.style.margin = "0px";
+        
     }else{
-        navbar.style.background = "white";
+        navbar.style.background = "#f8f9fa";
+        navbar.style.border = "2px solid #190844"
+        navbar.style.margin = "10px 40px";
     }
     console.log(scrollValue);
 }
